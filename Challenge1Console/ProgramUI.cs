9ea51a551f69ed2dca373831f96af030f48bc494
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-20 13:10:18 -0400

    Added AddItem method, Added several Helper Methods

--- Challenge1Console/ProgramUI.cs
+++ Challenge1Console/ProgramUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Challenge1Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,45 +10,91 @@ namespace Challenge1Console
 {
     public class ProgramUI
     {
-            bool runFlag = true;
+        bool runFlag = true;
+        MenuRepository Menu = new MenuRepository();
+
         public void Run()
         {
-            while (runFlag) 
+            Seed();
+            MainMenu();
+        }
+        private void MainMenu()
+        {
+            while (runFlag)
             {
+                Console.Clear();
+
                 Console.WriteLine("Welcome!\n Please Select from a list of options below:\n"
                     + "1. Add New Menu Item\n"
-                    + "2. Get Menu Items"
-                    + "3. Remove an Item from the menu"
-                    + "4. Exit the Program");
+                    + "2. Get Menu Items\n"
+                    + "3. Remove an Item from the menu\n"
+                    + "4. Exit the Program\n");
+
                 string userInput = Console.ReadLine().ToLower();
                 switch (userInput)
                 {
                     case string a when a.Contains("1"):
                     case string b when b.Contains("add"):
                     case string c when c.Contains("new"):
-                        Console.WriteLine("DEBUG((REMOVE LATER)) selection 1");
+                        AddItem();
                         break;
                     case string d when d.Contains("2"):
-                    case string e when e.Contains("menu"):
-                    case string f when f.Contains("get"):
+                    case string e when e.Contains("get"):
                         Console.WriteLine("DEBUG((REMOVE LATER)) selection 2");
                         break;
-                    case string g when g.Contains("3"):
-                    case string h when h.Contains("delete"):
-                    case string i when i.Contains("remove"):
+                    case string f when f.Contains("3"):
+                    case string g when g.Contains("delete"):
+                    case string h when h.Contains("remove"):
                         Console.WriteLine("DEBUG((REMOVE LATER)) selection 3");
                         break;
-                    case string j when j.Contains("4"):
-                    case string k when k.Contains("exit"):
-                    case string l when l.Contains("leave"):
-                        Console.WriteLine("DEBUG((REMOVE LATER)) selection 4");
+                    case string i when i.Contains("4"):
+                    case string j when j.Contains("exit"):
+                    case string k when k.Contains("leave"):
+                        Console.WriteLine("Exiting the program.");
                         Thread.Sleep(1000);
                         Exit();
                         break;
                 }
-            
+
             }
-            
+
+        }
+        private void AddItem()
+        {
+            Console.Clear();
+            Console.WriteLine("Add New Item Menu:\n");
+
+
+            Console.Write("Menu Number: ");
+            string userInput = Console.ReadLine();
+            var mealID = int.Parse(userInput);
+
+            Console.Write("Item Name: ");
+            userInput = Console.ReadLine();
+            string mealName = userInput;
+
+            Console.Write("Description: ");
+            userInput = Console.ReadLine();
+            string mealDesc = userInput;
+
+            Console.Write("Ingredients: ");
+            userInput = Console.ReadLine();
+            string mealIng = userInput;
+
+            Console.Write("Price: ");
+            userInput = Console.ReadLine();
+            var mealPrice = decimal.Parse(userInput);
+
+
+            MenuItem meal = new MenuItem(mealID, mealName, mealDesc, mealIng, mealPrice);
+            bool wasSuccessful = Menu.AddItemtoMenu(meal);
+            Console.WriteLine((wasSuccessful) ? "\nItem was successfully added." : "\nFailed to add Item.");
+
+            Continue();
+        }
+        private void PrintMenu()
+        {
+
         }
         private void Exit()
         {
@@ -57,6 +104,15 @@ namespace Challenge1Console
             Thread.Sleep(2000);
 
             runFlag = false;
+        }
+        private void Continue()
+        {
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+        }
+        private void Seed()
+        {
+
         }
     }
 }
