3ffd127d5239acf4196c124b40e7f96648293abf
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-20 10:05:01 -0400

    Added MenuItem Class to Library

--- Challenge1Library/Class1.cs
+++ Challenge1Library/Class1.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Challenge1Library
 {
-    public class Class1
+    public class MenuItem
     {
+        public int MealNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public decimal Price { get; set; }
+
+        public MenuItem() { }
+        public MenuItem(int mealNum, string name, string description, string ingredients, decimal price)
+        {
+            MealNumber = mealNum;
+            Name = name;
+            Description = description;
+            Ingredients = ingredients;
+            Price = price;
+        }
     }
 }
