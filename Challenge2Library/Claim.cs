7010f46df6b5c79b9b15bede77e14809a0bbd165
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-23 09:40:50 -0400

    Added Claim class and it's properties. Added Queue for Claims

--- Challenge2Library/Claim.cs
+++ Challenge2Library/Claim.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Challenge2Library
 {
+    public enum ClaimType { Car, Home, Theft}
     public class Claim
     {
+        public int ClaimID { get; set; }
+        public ClaimType ClaimType { get; set; }
+        public string Description { get; set; }
+        public decimal ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid 
+        { get
+            {
+                TimeSpan ClaimDifference = DateOfClaim - DateOfIncident;
+                if (ClaimDifference.Days <= 30)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            } 
+        }
+
+        public Claim() { }
+        public Claim(int id, ClaimType type, string desc, decimal amnt, DateTime incident, DateTime claim)
+        {
+            ClaimID = id;
+            ClaimType = type;
+            Description = desc;
+            ClaimAmount = amnt;
+            DateOfIncident = incident;
+            DateOfClaim = claim;
+        }
+        
     }
 }
---
+++ Challenge2Library/Queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2Library
+{
+    public class Queue
+    {
+        Queue<Claim> ClaimQueue = new Queue<Claim>();
+    }
+}
