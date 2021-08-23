4c1fd2a249a78930f7dc5bd6985910f665c79834
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-23 12:08:57 -0400

    Added Claim repository and Test for Repository Methods

---
+++ Challenge2Library/ClaimRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2Library
+{
+    public class ClaimRepository
+    {
+        Queue<Claim> _claimQueue = new Queue<Claim>();
+        Claim Claim = new Claim();
+
+
+        public bool AddtoQueue(Claim claim)
+        {
+            int startingCount = _claimQueue.Count;
+            _claimQueue.Enqueue(claim);
+            bool wasAdded = _claimQueue.Count > startingCount;
+            return wasAdded;
+        }
+        public bool RemoveFromQueue()
+        {
+            int startingCount = _claimQueue.Count;
+            _claimQueue.Dequeue();
+
+            bool wasRemoved = _claimQueue.Count < startingCount;
+            return wasRemoved;
+        }
+        public void SeeAllClaims()
+        {
+            //Kind of ugly looking, but I'll refactor it after the rest of the code is built out
+
+            Console.WriteLine($"{"Claim ID",-10} {"Type",-10} {"Description",-10} " +
+                $"{"Amount",10} {"Incident Date",10} {"Claim Date",10} {"Valid Claim",10}");
+            foreach(Claim item in _claimQueue)
+            {
+                Console.WriteLine($"{item.ClaimID,-10} {item.ClaimType,-10} {item.Description,-10} " +
+                $"{item.ClaimAmount,10} {item.DateOfIncident,10} {item.DateOfClaim,10} {item.IsValid,10}");
+            }
+        }
+        public void PeekQueue()
+        {
+            Claim query = _claimQueue.Peek();
+
+            Console.WriteLine($"Claim ID: {query.ClaimID}");
+            Console.WriteLine($"Claim Type: {query.ClaimType}");
+            Console.WriteLine($"Description: {query.Description}");
+            Console.WriteLine($"Amount: {query.ClaimAmount}");
+            Console.WriteLine($"Incident Date: {query.DateOfIncident.ToString("d")}");
+            Console.WriteLine($"Claim Date: {query.DateOfClaim.ToString("d")}");
+            Console.WriteLine($"Valid Claim: {query.IsValid}");
+        }
+
+    }
+}
--- Challenge2Tests/UnitTest1.cs
+++ Challenge2Tests/UnitTest1.cs
@@ -1,14 +1,57 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Challenge2Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Challenge2Tests
 {
     [TestClass]
     public class UnitTest1
     {
+
+        ClaimRepository claimRepository = new ClaimRepository();
+
+        Claim c1 = new Claim(1, ClaimType.Car, "Accident on 465.", 400.00m, new DateTime(2018, 4, 25), new DateTime(2018, 4, 27));
+        Claim c2 = new Claim(2, ClaimType.Home, "House fire in kitchen", 4000.00m, new DateTime(2018, 4, 11), new DateTime(2018, 4, 12));
+        Claim c3 = new Claim(3, ClaimType.Theft, "Stolen Pancakes", 4.00m, new DateTime(2018, 4, 27), new DateTime(2018, 6, 1));
+
+
+        
+
         [TestMethod]
-        public void TestMethod1()
+        public void ColumnPrintForClaims_ShouldPrintinColumns()
         {
+            claimRepository.AddtoQueue(c1);
+            claimRepository.AddtoQueue(c2);
+            claimRepository.AddtoQueue(c3);
+
+
+
+            claimRepository.SeeAllClaims();
+        }
+
+        [TestMethod]
+        public void Peeking()
+        {
+            claimRepository.AddtoQueue(c1);
+            claimRepository.AddtoQueue(c2);
+            claimRepository.AddtoQueue(c3);
+
+
+            claimRepository.PeekQueue();
+        }
+        [TestMethod]
+        public void Dequeue_ShouldReturnTrue()
+        {
+
+            claimRepository.AddtoQueue(c1);
+            claimRepository.AddtoQueue(c2);
+            claimRepository.AddtoQueue(c3);
+
+            bool wasSuccesful=claimRepository.RemoveFromQueue();
+            Assert.IsTrue(wasSuccesful);
+
+            claimRepository.PeekQueue();
         }
     }
 }
