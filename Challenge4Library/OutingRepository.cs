61541c440d5f4ccb998f236d335d3a8fa5a0c33d
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-25 14:13:40 -0400

    added methods to OutingRepository

---
+++ Challenge4Library/OutingRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge4Library
+{
+    public class OutingRepository
+    {
+        Outing _outing = new Outing();
+        List<Outing> _outingRepo = new List<Outing>();
+
+        public List<Outing> GetOut()
+        {
+            return _outingRepo;
+        }
+
+        public List<Outing> AllOutingsByType(EventType Event)
+        {
+            List<Outing> OutingByType = new List<Outing>();
+            foreach(Outing outing in _outingRepo)
+            {
+                if(outing.Event == Event)
+                {
+                    OutingByType.Add(outing);
+                }
+            }
+            return OutingByType;
+        }
+        public decimal GrandTotal()
+        {
+            decimal grandsum = 0;
+            foreach(Outing outing in _outingRepo)
+            {
+                grandsum += outing.TotalCost;
+            }
+            return grandsum;
+        }
+        public decimal GrandTotalByType(EventType Event)
+        {
+            List<Outing> OutingByType = new List<Outing>();
+            foreach (Outing outing in _outingRepo)
+            {
+                if (outing.Event == Event)
+                {
+                    OutingByType.Add(outing);
+                }
+            }
+            decimal grandsum = 0;
+            foreach(Outing outing in OutingByType)
+            {
+                grandsum += outing.TotalCost;
+            }
+            return grandsum;
+        }
+
+    }
+}
