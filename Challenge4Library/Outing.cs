3c327ca00133d7efebeb388146ae67b578a7a743
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-25 13:08:27 -0400

    Added outing class

--- Challenge4Library/Outing.cs
+++ Challenge4Library/Outing.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace Challenge4Library
 {
-    public enum EventType { Golf,Bowling, AmusementPark, Concert}
+    public enum EventType { Golf, Bowling, AmusementPark, Concert }
     public class Outing
     {
         public EventType Event { get; set; }
+        public decimal EventCost { get; set; }
+        public int Attendees { get; set; }
+        public decimal CostPerHead { get; set; }
+
+        public decimal CostOfAttendees
+        {
+            get
+            {
+                return Attendees * CostPerHead;
+            }
+        }
+        public DateTime DateOfEvent { get; set; }
+        public decimal TotalCost
+        {
+            get
+            {
+                return EventCost + CostOfAttendees;
+            }
+        }
+
+
 
     }
 }
