a50cc7bae2ee03aa388998ac6d95757fad17d6e6
Elijah Wood <elijah.v.wood@gmail.com>
2021-08-25 11:09:54 -0400

    Started Challenge 4

---
+++ Challenge4Library/Outing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge4Library
+{
+    public enum EventType { Golf,Bowling, AmusementPark, Concert}
+    public class Outing
+    {
+        public EventType Event { get; set; }
+
+    }
+}
---
+++ Challenge4Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge4Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge4Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("129a4392-692b-4dee-942c-b835ab021da1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
